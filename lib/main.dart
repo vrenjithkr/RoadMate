76db05bec6bcd163016309e2df602e423e7d47b7
vrenjithkr <vrenjithkr@gmail.com>
2022-07-10 22:12:44 +0530

    First commit -  till firebase varification

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'pages/RegisterScreen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    // home: StarterPage(),
+    home: RegisterScreen(),
+  ));
+}
